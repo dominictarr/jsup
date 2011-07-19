522859ef87f9f6963c442ea439482a72b3ff067e
James Halliday <mail@substack.net>
2011-07-19 02:37:38 -0700

    computing paths seemingly correctly

--- index.js
+++ index.js
@@ -2,23 +2,44 @@ var burrito = require('burrito');
 
 module.exports = function (src) {
     var self = {};
-    var nodes = [];
+    var obj = JSON.parse(src);
+    
+    var cursor = [ obj ];
+    var root = [ Object.create(Object.getPrototypeOf(obj)) ];
+    
+    var path = [];
+    var prevPathLen = 0;
     
     burrito('[\n' + src + '\n][0]', function (node) {
-        var p = node.parent();
-        if (p && p.name === 'object') {
-            var ix = this.path[ this.path.length - 2 ];
-            var key = p.value[0][ix][0];
-            console.log(key);
-        }
-        else if (p && p.name === 'array') {
+        if (this.path.length <= prevPathLen) {
+            path.pop();
+            prevPathLen = path.length;
         }
         
-        nodes.push(node);
+        var p = node.parent();
+        var key = undefined;
+        
+        if (!p) return
+        else if (p.name === 'object') {
+            var ix = this.path[ this.path.length - 2 ];
+            key = p.value[0][ix][0];
+        }
+        else if (p.name === 'array') {
+            key = this.key;
+        }
+        else if (p.name === 'sub' && node.value[0] !== 0) {
+            //console.dir(node.value);
+        }
+        else return;
+        
+        if (node.name === 'object' || node.name === 'array') {
+            prevPathLen = this.path.length;
+            path.push(key);
+        }
+        console.log(path.join('/'));
     });
     
     self.set = function (key, value) {
-        
     };
     
     self.toString = self.stringify = function () {
