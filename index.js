8cab588988c1fe0e5bdbc0f63166c6944311b154
James Halliday <mail@substack.net>
2011-07-19 02:44:19 -0700

    updating the cursor properly

--- index.js
+++ index.js
@@ -11,16 +11,18 @@ module.exports = function (src) {
     var prevPathLen = 0;
     
     burrito('[\n' + src + '\n][0]', function (node) {
-        if (this.path.length <= prevPathLen) {
-            path.pop();
-            prevPathLen = path.length;
-        }
-        
         var p = node.parent();
         var key = undefined;
         
         if (!p) return
-        else if (p.name === 'object') {
+        
+        if (this.path.length <= prevPathLen) {
+            path.pop();
+            cursor.shift();
+            prevPathLen = path.length;
+        }
+        
+        if (p.name === 'object') {
             var ix = this.path[ this.path.length - 2 ];
             key = p.value[0][ix][0];
         }
@@ -28,15 +30,19 @@ module.exports = function (src) {
             key = this.key;
         }
         else if (p.name === 'sub' && node.value[0] !== 0) {
-            //console.dir(node.value);
+            return; // the root
         }
         else return;
+        
+        //console.log(path.concat(key).join('/'));
         
         if (node.name === 'object' || node.name === 'array') {
             prevPathLen = this.path.length;
             path.push(key);
+console.log(key);
+console.dir(cursor[0]);
+            cursor.unshift(cursor[0][key]);
         }
-        console.log(path.join('/'));
     });
     
     self.set = function (key, value) {
